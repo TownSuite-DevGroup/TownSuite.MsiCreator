6dd387bc8d7044b50019888a51d3f5579a329ba0
Peter Gill <peter.gill@townsuite.com>
2025-08-01 16:53:34 -0230

    Add build script and update project file for tool packaging

---
+++ build.ps1
@@ -0,0 +1,13 @@
+#!/usr/bin/env powershell
+$ErrorActionPreference = "Stop"
+$CURRENTPATH=$pwd.Path
+
+# create the build output directory
+if (-not (Test-Path -Path "$CURRENTPATH/build")) {
+    New-Item -ItemType Directory -Path "$CURRENTPATH/build" | Out-Null
+}
+
+dotnet build -c "Release" -f "net8.0-windows"
+dotnet pack --configuration Release --no-build
+
+Get-ChildItem -Path $CURRENTPATH -Recurse -Filter "*.nupkg" | Where-Object { $_.FullName -notlike "*\build\*" } | Copy-Item -Destination "$CURRENTPATH/build" -Force
